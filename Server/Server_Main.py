e3b52184ead86da526dabefa553c83c077609bd2
Shalev Abadi <shalev787@gmail.com>
2019-04-11 14:03:41 +0300

    mongo and flask added

--- Server/Server_Main.py
+++ Server/Server_Main.py
@@ -1,1 +1,12 @@
-print("hello")
+from flask import Flask
+app = Flask(__name__)
+from flask_pymongo import PyMongo
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+app.run(debug=True)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
