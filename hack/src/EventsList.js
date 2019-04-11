ddbf763fc16b89fa1d6eae10892c61b9bed55b55
avivma <you@example.com>
2019-04-11 22:59:05 +0300

    list of events

--- hack/src/EventsList.js
+++ hack/src/EventsList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 //import './App.css';
+import EvCard from './EvCard';
+
 
 class EventsList extends Component {
 
@@ -14,24 +16,92 @@ class EventsList extends Component {
 
 
   render() {
+    var eventsArray = [{
+        "name":"running",
+        "description":"long run",
+        "evPic":"null",
+        "howFar":"5",
+        "attending":"6",
+        "required":"3"
+    },{
+        "name":"humusing",
+        "description":"eating humus",
+        "evPic":"null",
+        "howFar":"12",
+        "attending":"3",
+        "required":"3"
+        },{
+            "name":"humusing",
+            "description":"eating humus",
+            "evPic":"null",
+            "howFar":"12",
+            "attending":"3",
+            "required":"3"
+            },{
+                "name":"humusing",
+                "description":"eating humus",
+                "evPic":"null",
+                "howFar":"12",
+                "attending":"3",
+                "required":"3"
+                },{
+                    "name":"humusing",
+                    "description":"eating humus",
+                    "evPic":"null",
+                    "howFar":"12",
+                    "attending":"3",
+                    "required":"3"
+                    },{
+                        "name":"humusing",
+                        "description":"eating humus",
+                        "evPic":"null",
+                        "howFar":"12",
+                        "attending":"3",
+                        "required":"3"
+                        },{
+                            "name":"humusing",
+                            "description":"eating humus",
+                            "evPic":"null",
+                            "howFar":"12",
+                            "attending":"3",
+                            "required":"3"
+                            },{
+                                "name":"humusing",
+                                "description":"eating humus",
+                                "evPic":"null",
+                                "howFar":"12",
+                                "attending":"3",
+                                "required":"3"
+                                },{
+                                    "name":"humusing",
+                                    "description":"eating humus",
+                                    "evPic":"null",
+                                    "howFar":"12",
+                                    "attending":"3",
+                                    "required":"3"
+                                    }];
+       
+      // var obj=JSON.parse(eventsArray);
+        const s=[];
+        var i;
+      
+
+        var arraylength = eventsArray.length;
+      
+for(let i=0;i<arraylength;i++){
+    if(i%3===0){
+        s.push(<tr></tr>);
+    }
+s.push(<th><EvCard name={eventsArray[i].name} description={eventsArray[i].description} howFar={eventsArray[i].howFar}  attending={eventsArray[i].attending}/></th>);
+
+}
+       //var rendering = <EvCard name={eventsArray[0].name} description={eventsArray[0].description} howFar={eventsArray[0].howFar}  attending={eventsArray[0].attending}/>
     return (
         <div>
-        <table >
-        <tr>
-          <th>Firstname</th>
-          <th>Lastname</th> 
-          <th>Age</th>
-        </tr>
-        <tr>
-          <td>Jill</td>
-          <td>Smith</td> 
-          <td>50</td>
-        </tr>
-        <tr>
-          <td>Eve</td>
-          <td>Jackson</td> 
-          <td>94</td>
-        </tr>
+        <table>
+            <tbody>
+        {s}
+        </tbody>
       </table>
       </div>
     );
